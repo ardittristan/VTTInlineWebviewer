06c8e89318a9b3393c268e0eb07f6a876fb1293b
ardittristan <ardittristan@gmail.com>
2021-03-10 23:33:59 +0100

    fix workflow

--- .github/workflows/replace-url.js
+++ .github/workflows/replace-url.js
@@ -6,12 +6,12 @@ results.forEach((result) => {
   if (result.includes("inlineviewer.js")) {
     file = file.replace(
       /(<!-- insert_js_url_start -->).*(<!-- insert_js_url_end -->)/gsu,
-      `<!-- insert_js_url_start -->[inlineviewer.js](${result.split("=")[1]})<!-- insert_js_url_end -->`
+      `<!-- insert_js_url_start -->[inlineviewer.js](${result.match(/(?<==).*/)[0]})<!-- insert_js_url_end -->`
     );
   } else if (result.includes("inlineViewer.html")) {
     file = file.replace(
       /(<!-- insert_html_url_start -->).*(<!-- insert_html_url_end -->)/gsu,
-      `<!-- insert_html_url_start -->[inlineviewer.html](${result.split("=")[1]})<!-- insert_html_url_end -->`
+      `<!-- insert_html_url_start -->[inlineviewer.html](${result.match(/(?<==).*/)[0]})<!-- insert_html_url_end -->`
     );
   }
 });
